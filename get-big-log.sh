9982ad1ae5f3b59b4bd70af7c908dc60aadc53ab
Eric Hanchrow <eric.hanchrow@gmail.com>
2008-12-30 21:02:36 -0800

    v4 => bot

--- get-big-log.sh
+++ get-big-log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # copy the big log from the running rudybot, since it's great test data.
-rsync --progress --recursive linode:v4/big-log .
+rsync --progress --recursive linode:bot/big-log .
