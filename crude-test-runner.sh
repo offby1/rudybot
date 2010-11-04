ef3d04b035780d5eb4eae24ac339bc2ed0b71655
Eric Hanchrow <eric.hanchrow@gmail.com>
2010-11-03 19:58:46 -0700

    Like the name says ...

---
+++ crude-test-runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Sure be nice if there were a built-in way to run all the tests ...
+
+egrep -l schemeunit *.rkt | while read f
+do
+    if [ -x $f ]
+    then
+        echo "-----------$f--------------------"
+        ./$f
+    fi
+done
+
+echo "I hope you were paying attention to see if any of those tests failed, 'cuz I sure as hell wasn't"
