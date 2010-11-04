e782a33a7a75b9f3c8a9719a03436e16a5085f8a
Eric Hanchrow <eric.hanchrow@gmail.com>
2010-11-03 19:56:14 -0700

    Make it run!
    Looks like I haven't run the tests in a lonnnnng time ...

--- sighting-test.rkt
+++ sighting-test.rkt
@@ -6,9 +6,8 @@ exec  racket --require "$0" --main -- ${1+"$@"}
 (require (planet schematics/schemeunit:2/test)
          (planet schematics/schemeunit:2/text-ui)
          (planet schematics/schemeunit:2/util)
-         "userinfo.rkt")
+         (except-in "userinfo.rkt" main))
 
-(require/expose "userinfo.rkt" (*downcase-nicks*))
 
 
 (define sighting-tests
@@ -23,7 +22,7 @@ exec  racket --require "$0" --main -- ${1+"$@"}
       (check-equal? (lookup-sightings "snorkuplexity") '())))
    (test-case
     "persistent"
-    (parameterize ((*sightings-database-directory-name* "persistent-test.db"))
+    (parameterize ((*userinfo-database-directory-name* "persistent-test.db"))
       (let ((stuff (map make-sighting
                         (list "fred" "paul" "mary" "fred_")
                         (list "2" "3" "4" "5")
@@ -33,14 +32,14 @@ exec  racket --require "$0" --main -- ${1+"$@"}
                               (list "I" "am" "NOT" "dead")
                               (list "I" "am" "Jesus'" "mom")
                               (list "I'm" "fred" "with" "a" "trailing" "underscore")))))
-        (let ((writing? (not (directory-exists? (*sightings-database-directory-name*)))))
+        (let ((writing? (not (directory-exists? (*userinfo-database-directory-name*)))))
           ;; if the db doesn't exist, note some stuff.
           ;; if the db does exist, check for what we noted.
           (if writing?
             (printf "Putting test data into ~a; run me again and I'll check the contents.~%"
-                    (*sightings-database-directory-name*))
+                    (*userinfo-database-directory-name*))
             (printf "Reading test data from ~a~%"
-                    (*sightings-database-directory-name*)))
+                    (*userinfo-database-directory-name*)))
 
           (for ((s (in-list stuff)))
 
@@ -60,14 +59,13 @@ exec  racket --require "$0" --main -- ${1+"$@"}
             (looked-up-uc  (lookup-sightings "BOB")))
         (check-equal? looked-up-uc looked-up-lc)))
 
-    (parameterize ((*downcase-nicks* #f))
-      (delete-directory/files (build-path (*sightings-database-directory-name*) "bob"))
-      (let ((s (make-sighting "BOB" "2" 3 #f (list "hey" "you"))))
-        (note-sighting s)
-        (parameterize ((*downcase-nicks* #t))
-          (let ((looked-up-lc  (lookup-sightings "bob"))
-                (looked-up-uc  (lookup-sightings "BOB")))
-            (check-equal? looked-up-uc looked-up-lc))))))))
+    (delete-directory/files (build-path (*userinfo-database-directory-name*) "b" "bob"))
+
+    (let ((s (make-sighting "BOB" "2" 3 #f (list "hey" "you"))))
+      (note-sighting s)
+      (let ((looked-up-lc  (lookup-sightings "bob"))
+            (looked-up-uc  (lookup-sightings "BOB")))
+        (check-equal? looked-up-uc looked-up-lc))))))
 
 (define (main . args)
   (exit (test/text-ui sighting-tests 'verbose)))
