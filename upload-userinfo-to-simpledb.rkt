c68981f3816cc248b8aa3230a711accdefa4cd9f
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-05-22 14:15:20 -0700

    Gotta start somewhere, as I always say.

---
+++ upload-userinfo-to-simpledb.rkt
@@ -0,0 +1,33 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
+|#
+
+#lang racket
+(require (except-in "userinfo.rkt" main))
+
+(define (unwrap thing)
+  (match thing
+    [(list (list 'sightings s ...) _ ...) s]))
+
+(define (contents p)
+  (call-with-input-file p read))
+
+(define (snarf-userinfo)
+  (if (directory-exists? (*userinfo-database-directory-name*))
+      (map (lambda (fn)
+             (unwrap (contents fn)))
+           (find-files file-exists? (*userinfo-database-directory-name*)))
+      '()))
+
+(provide main)
+(define (main . args)
+  (printf "Hi~%")
+  (call-with-output-file
+      "/tmp/whee"
+    (lambda (op)
+      (for ([sighting-list (snarf-userinfo)])
+        (for ([s sighting-list])
+        (write s op)
+        (newline op))))
+    #:exists 'replace))
