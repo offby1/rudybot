274713332ff81f1699bdc2a85c3909ebed6b7ea4
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-04 13:34:21 -0700

    finish simplifaction

--- upload-log-to-simpledb.rkt
+++ upload-log-to-simpledb.rkt
@@ -17,16 +17,13 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
 
 (define (trim-action utterance)
   (match utterance
-   [(regexp #px"^\1ACTION (.*)\1$" (list _ action))
-    (format "doing ~a" action)]
-   [_ (format "saying ~a" utterance)]))
+    [(regexp #px"^\1ACTION (.*)\1$" (list _ action))
+     ;; actions are generally in the present tense, e.g. "backs away
+     ;; slowly".  I wish I could convert to past tense here, but alas.
+     (format ": ~a" (bytes->string/utf-8 action))]
+    [_ (format "saying ~s" (bytes->string/utf-8 utterance))]))
 
-(check-equal? (trim-action "frotz")
-              "frotz")
-(check-equal? (trim-action "\1ACTION you said it\1")
-              "you said it")
-
-(define (message->description m)
+(define (timestamp+message->description m)
   (match m
     [(list timestamp
            (list 'prefix prefix)
@@ -35,62 +32,62 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
 
      (let* ([command-str (bytes->string/utf-8 command)])
        (case (string->symbol command-str)
-         ((JOIN PART)
-          (format "~aing ~a" (string-downcase command-str) (second (first params))))
+         ((JOIN)
+          (format "~aing ~a"      (string-downcase command-str) (second (first params))))
+         ((PART)
+          (format "~aing from ~a" (string-downcase command-str) (second (first params))))
          ((QUIT)
-          (format "quitting, mumbling ~s" (bytes->string/utf-8 (second (first params)))))
+          (let ([parting-words (second (first params))])
+            (if (bytes=? #"" parting-words)
+                (format "quitting")
+                (format "quitting, saying ~s" (bytes->string/utf-8 parting-words)))))
          ((PRIVMSG)
-          (format "~a in ~a" (second (second params))
-                  (trim-action
-                   (second (first params))) ))))]
+          (format "~a ~a" (trim-action
+                           (second (second params)))
+                  (second (first params)) ))))]
     [_ 'wtf]))
 
-(define (log-entry->sighting s)
-  ;; stuff to pull a complete log entry -- including timestamp and
-  ;; "<=" -- apart
-  )
+(define-test-suite tests
+  (check-equal? (trim-action #"frotz")
+                "saying \"frotz\"")
+  (check-equal? (trim-action #"\1ACTION you said it\1")
+                ": you said it")
+  (check-equal? (timestamp+message->description
+                 '("123.456"
+                   (prefix #"colonel Mustard")
+                   (command #"PRIVMSG")
+                   (params (param #"in the study")
+                           (param #"\1ACTION whuppin' hisself on de haid wit de candlestick\1"))))
+                ": whuppin' hisself on de haid wit de candlestick in the study")
+  (check-equal? (timestamp+message->description '("123.456"
+                                                  (prefix #"colonel Mustard")
+                                                  (command #"PRIVMSG")
+                                                  (params (param #"in the study")
+                                                          (param #"gimme seltzer"))))
+                "saying \"gimme seltzer\" in the study")
 
-(check-equal? (message->description '("123.456"
-                                   (prefix #"colonel Mustard")
-                                   (command #"PRIVMSG")
-                                   (params (param #"in the study")
-                                           (param #"\1ACTION whuppin' hisself on de haid wit de candlestick\1"))))
-              (sighting "colonel Mustard"
-                        "in the study"
-                        123.456
-                        #t
-                        "whuppin' hisself on de haid wit de candlestick"))
+  (check-equal? (timestamp+message->description '("123.456"
+                                                  (prefix #"rudybot!~luser@ec2-204-236-167-175.us-west-1.compute.amazonaws.com")
+                                                  (command #"PART")
+                                                  (params (param #"#scheme"))))
+                "parting from #scheme")
 
-(check-equal? (message->description '("123.456"
-                                   (prefix #"colonel Mustard")
-                                   (command #"PRIVMSG")
-                                   (params (param #"in the study")
-                                           (param #"gimme seltzer"))))
-              (sighting "colonel Mustard"
-                        "in the study"
-                        123.456
-                        #f
-                        "gimme seltzer"))
+  (check-equal? (timestamp+message->description '("123.456"
+                                                  (prefix #"monqy")
+                                                  (command #"QUIT")
+                                                  (params (param #"Quit: can't see a thing"))))
+                "quitting, saying \"Quit: can't see a thing\"")
 
-(check-equal? (message->description '("123.456"
-                                   (prefix #"rudybot!~luser@ec2-204-236-167-175.us-west-1.compute.amazonaws.com")
-                                   (command #"PART")
-                                   (params (param #"#scheme"))))
-              (sighting "rudybot!~luser@ec2-204-236-167-175.us-west-1.compute.amazonaws.com"
-                        "#scheme"
-                        123.456
-                        #f
-                        "PART"))
+  (check-equal? (timestamp+message->description
+                 '("123.456"
+                   (prefix #"monqy")
+                   (command #"QUIT")
+                   (params (param #""))))
+                "quitting"))
 
-(check-equal? (message->description '("123.456"
-                                   (prefix #"monqy")
-                                   (command #"QUIT")
-                                   (params (param #"Quit: can't see a thing"))))
-              (sighting "monqy"
-                        ""
-                        123.456
-                        #f
-                        "Quit: can't see a thing"))
+(when (positive?  (run-tests tests))
+  (exit 1))
+
 (provide main)
 (define (main . args)
   "a-ok")
