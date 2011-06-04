670fdcfa0131b0a6de39752b78aef3b4c88a152f
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-04 13:14:15 -0700

    In the middle of simplifying all this

--- upload-log-to-simpledb.rkt
+++ upload-log-to-simpledb.rkt
@@ -6,57 +6,51 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
 #lang racket
 
 (require racket/trace
-         rackunit rackunit/text-ui
-         (except-in "userinfo.rkt" main))
+         rackunit rackunit/text-ui)
+
+(struct sighting (who when description) #:prefab)
 
 ;; grovel log, pulling out new-style sexps (and associated timestamps)
 ;;   actually, only get those whose command is JOIN, PART, QUIT, or
 ;;   PRIVMSG
-;; come up with a struct sighting: who where when action? words
+;; come up with a struct sighting
 
 (define (trim-action utterance)
   (match utterance
    [(regexp #px"^\1ACTION (.*)\1$" (list _ action))
-    action]
-   [_ utterance]))
+    (format "doing ~a" action)]
+   [_ (format "saying ~a" utterance)]))
 
 (check-equal? (trim-action "frotz")
               "frotz")
 (check-equal? (trim-action "\1ACTION you said it\1")
               "you said it")
 
-(define (message->sighting m)
+(define (message->description m)
   (match m
     [(list timestamp
            (list 'prefix prefix)
            (list 'command command)
            (list 'params params ...))
 
-     (let* ([str (bytes->string/utf-8 command)]
-            [sym (string->symbol str)])
-       (case sym
+     (let* ([command-str (bytes->string/utf-8 command)])
+       (case (string->symbol command-str)
          ((JOIN PART)
-          (sighting (bytes->string/utf-8 prefix)                  ;who
-                    (bytes->string/utf-8 (second (first params))) ;where
-                    (string->number timestamp) ;when
-                    #f                         ;action?
-                    str                        ;words
-                    ))
+          (format "~aing ~a" (string-downcase command-str) (second (first params))))
          ((QUIT)
-          (sighting (bytes->string/utf-8 prefix)
-                    ""
-                    (string->number timestamp)
-                    #f
-                    (bytes->string/utf-8 (second (first params)))))
+          (format "quitting, mumbling ~s" (bytes->string/utf-8 (second (first params)))))
          ((PRIVMSG)
-          (sighting (bytes->string/utf-8 prefix)
-                    (bytes->string/utf-8 (second (first params)))
-                    (string->number timestamp)
-                    (not (not (regexp-match "^\1ACTION " (second (second params)))))
-                    (bytes->string/utf-8 (trim-action (second (second params))))))))]
+          (format "~a in ~a" (second (second params))
+                  (trim-action
+                   (second (first params))) ))))]
     [_ 'wtf]))
 
-(check-equal? (message->sighting '("123.456"
+(define (log-entry->sighting s)
+  ;; stuff to pull a complete log entry -- including timestamp and
+  ;; "<=" -- apart
+  )
+
+(check-equal? (message->description '("123.456"
                                    (prefix #"colonel Mustard")
                                    (command #"PRIVMSG")
                                    (params (param #"in the study")
@@ -67,7 +61,7 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
                         #t
                         "whuppin' hisself on de haid wit de candlestick"))
 
-(check-equal? (message->sighting '("123.456"
+(check-equal? (message->description '("123.456"
                                    (prefix #"colonel Mustard")
                                    (command #"PRIVMSG")
                                    (params (param #"in the study")
@@ -78,7 +72,7 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
                         #f
                         "gimme seltzer"))
 
-(check-equal? (message->sighting '("123.456"
+(check-equal? (message->description '("123.456"
                                    (prefix #"rudybot!~luser@ec2-204-236-167-175.us-west-1.compute.amazonaws.com")
                                    (command #"PART")
                                    (params (param #"#scheme"))))
@@ -88,7 +82,7 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
                         #f
                         "PART"))
 
-(check-equal? (message->sighting '("123.456"
+(check-equal? (message->description '("123.456"
                                    (prefix #"monqy")
                                    (command #"QUIT")
                                    (params (param #"Quit: can't see a thing"))))
