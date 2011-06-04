855ad9247cfc9c0ab90a5f374fc525ca9be6ec8c
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-06-04 13:13:25 -0700

    Fix hostname, and throw in --partial while we're at it

--- get-big-log.sh
+++ get-big-log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # copy the big log from the running rudybot, since it's great test data.
-rsync --progress --recursive xen:/usr/local/src/rudybot/big-log .
+rsync --partial --progress --recursive ec2:/usr/local/src/rudybot/big-log .
