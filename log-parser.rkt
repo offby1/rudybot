66e01ea0409b3d75514bd57a8781cd80a615f4a4
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-11-12 12:50:25 -0800

    Rewrite to output to sqlite db, rather than a plain file.
    Hopefully, if incubot reads from this db, it will be able to choose
    randomly from the complete log, reasonably quickly, and without having
    to snarf a subset into RAM.
    
    Seems to work.

--- log-parser.rkt
+++ log-parser.rkt
@@ -7,15 +7,19 @@ exec racket --require "$0" --main -- ${1+"$@"}
 #lang racket
 
 (require
- (only-in profile profile-thunk)
  (only-in mzlib/etc this-expression-source-directory)
+ (only-in db
+          query-exec
+          query-rows
+          sqlite3-connect
+          )
  (planet schematics/schemeunit:3)
  (planet schematics/schemeunit:3/text-ui) )
 
 (provide (struct-out utterance))
 (struct utterance (timestamp speaker target text) #:prefab)
 
-(define (string->utterance s)
+(define (log-file-string->utterance s)
   (define (ensure-string x)
     (cond
      [(string? x)
@@ -44,7 +48,7 @@ exec racket --require "$0" --main -- ${1+"$@"}
   (for ([line '("2010-01-19T03:01:31Z <= \":offby1!n=user@pdpc/supporter/monthlybyte/offby1 PRIVMSG ##cinema :rudybot: uptime\""
                 "2010-01-19T03:01:31Z <= ((prefix #\"offby1!n=user@pdpc/supporter/monthlybyte/offby1\") (command #\"PRIVMSG\") (params (param #\"##cinema\") (param #\"rudybot: uptime\")))")])
 
-    (check-equal? (string->utterance line)
+    (check-equal? (log-file-string->utterance line)
                   #s(utterance "2010-01-19T03:01:31Z"
                                "offby1"
                                "##cinema"
@@ -53,6 +57,48 @@ exec racket --require "$0" --main -- ${1+"$@"}
 
 (define (pe fmt . args)
   (apply fprintf (current-error-port) fmt args))
+
+;; Our own little ORM
+(define (create-db)
+  (let ([connection
+         (sqlite3-connect
+          #:database "/tmp/parsed-log.db"
+          #:mode 'create)])
+
+    ;; The primary key is to prevent duplicates when we're
+    ;; bulk-loading the table.  But it's imperfect, since there may
+    ;; occasionally be two utterances from the same speaker, to the
+    ;; same target, at the same time.  So ON CONFLICT discards all but
+    ;; the newest.  Hopefully this won't happen too often.
+
+    ;; I imagine that FAIL, REPLACE, and IGNORE would all work equally
+    ;; well here.
+    (query-exec
+     connection
+     "CREATE TABLE IF NOT EXISTS
+        log(timestamp TEXT, speaker TEXT, target TEXT, text TEXT,
+            PRIMARY KEY (timestamp, speaker, target)
+            ON CONFLICT FAIL)")
+    connection))
+(define (insert db u)
+  (query-exec
+   db
+   "insert into log values (?, ?, ?, ?)"
+   (utterance-timestamp u)
+   (utterance-speaker   u)
+   (utterance-target    u)
+   (utterance-text      u)))
+
+(define (begin-transaction db)
+  (query-exec db "BEGIN TRANSACTION"))
+
+(define (end-transaction db)
+  (query-exec db "COMMIT"))
+
+(define (current-line ip)
+  (call-with-values
+      (thunk (port-next-location ip))
+    (lambda (line col pos) line)))
 
 (provide main)
 (define (main . args)
@@ -69,20 +115,22 @@ exec racket --require "$0" --main -- ${1+"$@"}
     (error 'log-parser "I want at most one input file name; instead you gave me ~s" input-file-names)]
    [else
     (let ([input-file-name (build-path (this-expression-source-directory) (car input-file-names))]
-          [output-file-name "parsed-log"])
+          [db (create-db)])
       (call-with-input-file
           input-file-name
         (lambda (ip)
-          (pe "Reading from ~a; writing to ~a..." input-file-name output-file-name)
           (port-count-lines! ip)
-          (profile-thunk
-           (lambda ()
-             (call-with-output-file output-file-name
-               (lambda (op)
-                 (for ([line (in-lines ip)])
-                   (let ([utz (string->utterance line)])
-                     (when utz
-                       (write utz op)
-                       (newline op)))))
-               #:exists 'truncate)))))
+
+          (begin-transaction db)
+          (for ([line (in-lines ip)])
+            (cond
+             ((log-file-string->utterance line) => (lambda (ut) (insert db ut))))
+
+            (when (zero? (remainder (current-line ip) 2000))
+              (end-transaction db)
+              (begin-transaction db)
+              (fprintf (current-error-port)
+                       "Line ~a~%"
+                       (current-line ip))))
+          (end-transaction db)))
       (pe "done~%"))]))
