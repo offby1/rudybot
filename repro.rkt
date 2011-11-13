b321d80f4f92f6fc7bfd980265813d6cbba9697b
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-11-12 16:09:21 -0800

    This repros

---
+++ repro.rkt
@@ -0,0 +1,29 @@
+;; rm -rfv /tmp/bug* ; racket ./repro.rkt
+
+#lang racket
+
+(require
+ (only-in db
+          query-exec
+          query-value
+          sqlite3-connect))
+
+(define db
+  (sqlite3-connect #:database "/tmp/buggissimo" #:mode 'create))
+
+(query-exec
+ db
+ "CREATE TABLE IF NOT EXISTS
+        log_word_map(word TEXT, log_id INTEGER,
+            PRIMARY KEY (word, log_id)
+            ON CONFLICT FAIL)")
+
+(query-exec db "BEGIN TRANSACTION")
+
+(for ([x (in-range 20000)])
+  (query-exec
+   db
+   "insert into log_word_map values (?, ?)"
+   (number->string x) 99))
+
+(query-exec db "COMMIT")
--- utterance.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket
-
-(provide (struct-out utterance))
-(struct utterance (timestamp speaker target text) #:prefab)
