ed097af84e638a0cab2f14b45f09323ee93376a5
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-03-16 12:58:11 -0700

    compress!  Geez, it's a lot faster this way.

--- get-big-log.sh
+++ get-big-log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # copy the big log from the running rudybot, since it's great test data.
-rsync --progress --recursive ec2:/mnt/rudybot/big-log .
+rsync --progress --recursive --compress --partial ec2:/mnt/rudybot/big-log .
