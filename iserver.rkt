5abaa4bcc2966124bbf3e5c14bdeacaf021b601c
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-03-17 16:42:36 -0700

    Fix (in a rather awful way) a nasty subtle thread-sync bug

--- iserver.rkt
+++ iserver.rkt
@@ -31,8 +31,22 @@
                    (thread-send client-thread (incubot-sentence string c))
                    c)
                   ((put-string)
-                   (thread-send client-thread #t)
-                   (add-string-to-corpus string c))
+                   ;; BUGBUG -- OK, this is INSANELY fragile.  Turns
+                   ;; out that we must ensure that the call to thread-send
+                   ;; comes after the call to add-string-to-corpus,
+                   ;; even though we want to return the value of the
+                   ;; latter, since add-string-to-corpus might fail,
+                   ;; and if it does, the exception handler above will
+                   ;; push out a #f _in addition to_ the #t that we
+                   ;; (mistakenly) already pushed out.  And when that
+                   ;; happens, the client gets out of sync with us.
+
+                   ;; So this whole idea of using mailboxes to
+                   ;; communicate betwixt client and server needs to
+                   ;; be rethought.
+                   (begin0
+                       (add-string-to-corpus string c)
+                     (thread-send client-thread #t)))
                   (else
                    (log "Unknown verb ~s" verb)
                    (thread-send client-thread #f)
