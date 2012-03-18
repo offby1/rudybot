881061679eb3957c1b02cf9366b8dfefde474277
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-03-18 16:55:31 -0700

    Fix the insane fragility.

--- iserver.rkt
+++ iserver.rkt
@@ -21,36 +21,25 @@
         (let ([message (thread-receive)])
           (match message
             [(list client-thread verb string)
-             (loop
-              (with-handlers ([exn? (lambda (e)
-                                      (log "Drat: ~a" e)
-                                      (thread-send client-thread #f)
-                                      (loop c))])
-                (case verb
-                  ((get)
-                   (thread-send client-thread (incubot-sentence string c))
-                   c)
-                  ((put-string)
-                   ;; BUGBUG -- OK, this is INSANELY fragile.  Turns
-                   ;; out that we must ensure that the call to thread-send
-                   ;; comes after the call to add-string-to-corpus,
-                   ;; even though we want to return the value of the
-                   ;; latter, since add-string-to-corpus might fail,
-                   ;; and if it does, the exception handler above will
-                   ;; push out a #f _in addition to_ the #t that we
-                   ;; (mistakenly) already pushed out.  And when that
-                   ;; happens, the client gets out of sync with us.
-
-                   ;; So this whole idea of using mailboxes to
-                   ;; communicate betwixt client and server needs to
-                   ;; be rethought.
-                   (begin0
-                       (add-string-to-corpus string c)
-                     (thread-send client-thread #t)))
-                  (else
-                   (log "Unknown verb ~s" verb)
-                   (thread-send client-thread #f)
-                   c))))]
+             (call-with-values
+                 (thunk
+                  (with-handlers ([exn? (lambda (e)
+                                          (log "Drat: ~a" e)
+                                          (values #f c))])
+                    (case verb
+                      ((get)
+                       (values
+                        (incubot-sentence string c)
+                        c))
+                      ((put-string)
+                       (values #t
+                               (add-string-to-corpus string c)))
+                      (else
+                       (log "Unknown verb ~s" verb)
+                       (values #f c)))))
+               (lambda (response corpus)
+                 (thread-send client-thread response)
+                 (loop corpus)))]
             [(list client-thread ...)
              (log  "Got unexpected message ~s" message)
              (thread-send client-thread #f)
