f334cd8adc6b2df6c3075247752fc3f1d1ce007c
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-10-28 23:28:17 +0000

    Yikes.  22 million rows to process, and we're only doing maybe 20 per second.

--- regenerate-log-word-map.rkt
+++ regenerate-log-word-map.rkt
@@ -18,10 +18,13 @@ exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
   (let ([conn (db:sqlite3-connect
                #:database "/tmp/corpus.db"
                #:mode 'read/write)])
-    (for ([(id row) (db:in-query
-                     conn
-                     "select rowid, text from log"
-                     #:fetch 10)])
-      (for ([word (string->lowercased-words row)])
-        (db:query-exec conn "insert into new_log_word_map values ($1, $2)"
-                       word id)))))
+    (let ([rows-to-process (db:query-value conn "select count(*) from log")])
+      (for ([(id row) (db:in-query
+                       conn
+                       "select rowid, text from log"
+                       #:fetch 1000)])
+        (displayln rows-to-process)
+        (for ([word (string->lowercased-words row)])
+          (db:query-exec conn "insert into new_log_word_map values ($1, $2)"
+                         word id))
+        (set! rows-to-process (sub1 rows-to-process))))))
