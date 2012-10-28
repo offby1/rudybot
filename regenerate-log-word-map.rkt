b63b8ef5b410a374e0ebe6af835b3bfdd1f06e0b
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-10-28 23:09:10 +0000

    Beginning of a one-shot tool ...
    ... that might fix the bug described in bugs/nick-in-incubot-spew

---
+++ regenerate-log-word-map.rkt
@@ -0,0 +1,25 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+exec racket -l errortrace --require "$0" --main -- ${1+"$@"}
+|#
+
+#lang racket
+(require "corpus.rkt"
+         (prefix-in db: db))
+
+;; open the db
+;; for each row in the "log" table ...
+;;   split the string into words using wordlist->wordset
+;;   for each of those words ...
+;;      create a new entry in log-word-map consisting of this word, and the rowid from "log"
+
+(provide main)
+(define (main . args)
+  (let ([conn (db:sqlite3-connect
+               #:database "/tmp/corpus.db"
+               #:mode 'read/write)])
+    (for ([row (db:in-query
+                conn
+                "select text from log"
+                #:fetch 10)])
+      (displayln row))))
