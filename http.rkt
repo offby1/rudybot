7b5ffd982fe345eb50a1338475dac51abe27c73d
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-09-14 15:41:35 -0700

    Move stuff around to clarify ...
    ... there's actually very little real code here; the bulk of the file
    is tests.

--- http.rkt
+++ http.rkt
@@ -12,59 +12,15 @@
 
 (provide (struct-out exn:fail:http))
 (struct exn:fail:http exn:fail (code))
-(struct http-response (code message seconds mime headers body) #:transparent)
 
-(define (split-headers inp)
-  (match (port->string inp)
-    [(regexp #px"(.*?)\r\n\r\n(.*)" (list _ header-string body-string))
-     (list header-string body-string)]))
+(provide post-pure-port/gack)
+;; Like post-pure-port, but raises an exception if the HTTP response
+;; is anything other than 200.
+(define (post-pure-port/gack url post [header null])
+  (let ([inp (post-impure-port url post header)])
+    (check-and-discard-status-and-header-lines inp)
+    inp))
 
-
-(define (header-dict)
-  (make-immutable-custom-hash string-ci=?
-                              (compose equal-hash-code string-downcase)))
-
-(define (lines->hash lines)
-  (apply dict-set* (header-dict)
-         (flatten (map (lambda (l)
-                         (match l
-                           [(regexp "(.*?): *(.*)" (list _ key value)) (list key value)]))
-                       lines))))
-
-(define (port->http-response inp)
-  (match-define (list header-string body)
-                (split-headers inp))
-  (match-define
-   (list status-line header-lines ...)
-    (regexp-split "\r\n" header-string))
-
-  (match-define
-   (list _ http-version status-code explanation)
-   (regexp-match #px"(HTTP/.*?) ([0-9]{3}) (.*)" status-line))
-
-  (http-response (string->number status-code) explanation 0 "mime? wozzat?"
-                 (lines->hash header-lines)
-                 body))
-
-(module+ test
-  (define 404-response-string "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\nFrotz: plotz\r\nDate: Sun, 04 Aug 2013 21:29:18 GMT\r\n\r\nHey You!!")
-  (define r (port->http-response (open-input-string 404-response-string)))
-  (check-equal? (http-response-code r)    404)
-  (check-equal? (http-response-message r) "Not Found")
-  (let ([headers (http-response-headers r)])
-    (check-equal? (dict-ref headers "Content-Type") "text/html")
-    (check-equal? (dict-ref headers "frotz"       ) "plotz")
-    (check-equal? (dict-ref headers "date"        ) "Sun, 04 Aug 2013 21:29:18 GMT")
-    )
-  (check-equal? (http-response-body r) "Hey You!!"))
-
-(module+ test
-  (check-equal? (split-headers (open-input-string "frotz\r\n\r\nplotz\nmore\r\neven more"))
-                '("frotz" "plotz\nmore\r\neven more")))
-
-;; BUGBUG -- doesn't handle "folded" header lines -- those that begin
-;; with a space or a tab.  See
-;; http://www.w3.org/Protocols/rfc2616/rfc2616-sec2.html#sec2.2
 (define (check-and-discard-status-and-header-lines inp)
   (define status-line (read-bytes-line inp 'return-linefeed))
 
@@ -84,7 +40,53 @@
 
   inp)
 
+;; BUGBUG -- doesn't handle "folded" header lines -- those that begin
+;; with a space or a tab.  See
+;; http://www.w3.org/Protocols/rfc2616/rfc2616-sec2.html#sec2.2
 (module+ test
+  (struct http-response (code message seconds mime headers body) #:transparent)
+  (define (split-headers inp)
+    (match (port->string inp)
+      [(regexp #px"(.*?)\r\n\r\n(.*)" (list _ header-string body-string))
+       (list header-string body-string)]))
+
+
+  (define (header-dict)
+    (make-immutable-custom-hash string-ci=?
+                                (compose equal-hash-code string-downcase)))
+
+  (define (lines->hash lines)
+    (apply dict-set* (header-dict)
+           (flatten (map (lambda (l)
+                           (match l
+                             [(regexp "(.*?): *(.*)" (list _ key value)) (list key value)]))
+                         lines))))
+  (define (port->http-response inp)
+    (match-define (list header-string body)
+                  (split-headers inp))
+    (match-define
+     (list status-line header-lines ...)
+     (regexp-split "\r\n" header-string))
+
+    (match-define
+     (list _ http-version status-code explanation)
+     (regexp-match #px"(HTTP/.*?) ([0-9]{3}) (.*)" status-line))
+
+    (http-response (string->number status-code) explanation 0 "mime? wozzat?"
+                   (lines->hash header-lines)
+                   body))
+  (define 404-response-string "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\nFrotz: plotz\r\nDate: Sun, 04 Aug 2013 21:29:18 GMT\r\n\r\nHey You!!")
+  (define r (port->http-response (open-input-string 404-response-string)))
+  (check-equal? (http-response-code r)    404)
+  (check-equal? (http-response-message r) "Not Found")
+  (let ([headers (http-response-headers r)])
+    (check-equal? (dict-ref headers "Content-Type") "text/html")
+    (check-equal? (dict-ref headers "frotz"       ) "plotz")
+    (check-equal? (dict-ref headers "date"        ) "Sun, 04 Aug 2013 21:29:18 GMT")
+    )
+  (check-equal? (http-response-body r) "Hey You!!")
+  (check-equal? (split-headers (open-input-string "frotz\r\n\r\nplotz\nmore\r\neven more"))
+                '("frotz" "plotz\nmore\r\neven more"))
   (check-equal? (port->string (check-and-discard-status-and-header-lines (open-input-bytes #"HTTP/2.3 200 Eva thang funky\r\nNow what\r\nFoo: Bar\r\nLong Line\r\n\tcontinuation\r\n\r\nBody!\nMore Breck Body!!\r\n")))
                 "Body!\nMore Breck Body!!\r\n")
   (check-exn
@@ -93,11 +95,3 @@
           (= 201 (exn:fail:http-code e))
           (string=? "Outlook not so good" (exn-message e))))
    (thunk (check-and-discard-status-and-header-lines (open-input-bytes #"HTTP/2.3 201 Outlook not so good")))))
-
-(provide post-pure-port/gack)
-;; Like post-pure-port, but raises an exception if the HTTP response
-;; is anything other than 200.
-(define (post-pure-port/gack url post [header null])
-  (let ([inp (post-impure-port url post header)])
-    (check-and-discard-status-and-header-lines inp)
-    inp))
