349556ffc2fd67f50be6c0a2a12c825f8e60368a
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-08-21 16:27:45 -0700

    Quickly choose a random row for each token

--- spike.py
+++ spike.py
@@ -1,16 +1,29 @@
-import collections
 import sqlite3
 
 
 def tokenize(sentence):
     with sqlite3.connect('corpus.db') as conn:
-        hits_by_token = collections.defaultdict(set)
+        hits_by_token = []
         for token in sentence.strip().split():
 
-            for row in conn.execute("SELECT text FROM f_log WHERE text match ? LIMIT 10", (token,)):
-                hits_by_token[token].add(row)
+            random_offset = next(conn.execute("""select abs(random()) % (max(rowid)) from f_log"""))[0]
+
+            # I suspect that sqlite interprets certain upper-case
+            # words like AND as syntax
+            token = token.lower()
+
+            for text in conn.execute("""
+            SELECT text
+            FROM f_log
+            WHERE text MATCH ?
+            AND rowid > ?
+            ORDER BY rowid ASC
+            LIMIT 1
+            """,
+                                    (token, random_offset)):
+                hits_by_token.append((token, text))
 
     return hits_by_token
 
 import pprint
-pprint.pprint(dict(tokenize("The rain in Spain falls mainly in the #emacs")))
+pprint.pprint(tokenize("The rain in Spain falls mainly in the #emacs"), width=250)
