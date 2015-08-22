06349cef8747b44d62d746ee51f8ea16953dcfa0
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-08-21 21:36:41 -0700

    Do add-sentence

--- iserver.rkt
+++ iserver.rkt
@@ -1,15 +1,31 @@
-#lang racket
+#lang at-exp racket
 
 (require
+ (prefix-in db: db)
+ (only-in mzlib/etc this-expression-source-directory)
  (only-in "vars.rkt" *incubot-logger*)
  unstable/debug)
 
+(define *db-file-name*
+  (make-parameter
+   (build-path (this-expression-source-directory)
+               "corpus.db")))
+
+(define (add-sentence conn s)
+   (db:query-exec
+    conn
+    "INSERT INTO f_log VALUES (?)"
+    s))
+
 (provide make-incubot-server)
 (define (make-incubot-server)
+  (define connection (db:sqlite3-connect
+                      #:database (*db-file-name*)))
+  (dprintf "Connected to ~a: ~a" (*db-file-name*) connection)
   (lambda (command-sym inp)
     (match command-sym
       ['put-string
-       (dprintf "Pretend I'm saving ~s somewhere" inp)]
+       (add-sentence connection inp)]
       ['get
        (dprintf "Pretend I'm retrieving something from ~a" inp)
        "incubot is on vacation today"])
