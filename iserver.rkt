8a24c860d6695291abfe916c9e6ee8284269d775
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-08-21 22:11:31 -0700

    Damn, looks like it's working.

--- iserver.rkt
+++ iserver.rkt
@@ -17,6 +17,44 @@
     "INSERT INTO f_log VALUES (?)"
     s))
 
+
+(define (rarest-token conn s)
+  (for/fold ([token "wtf?"]
+             [popularity +inf.0])
+      ([(this-token this-popularity)
+        (db:in-query conn
+                     @string-append{
+                                    SELECT  tok1.token, ft_terms.occurrences
+                                    FROM    tok1
+                                    JOIN    ft_terms ON ft_terms.term = tok1.token
+                                    WHERE   INPUT=?
+                                    AND     ft_terms.col = '*'
+                                    }
+                     s)])
+    (if (< this-popularity popularity)
+        (values this-token this-popularity)
+        (values token popularity))))
+
+
+(define (random-offset conn)
+  (db:query-value conn
+                  "select abs(random()) % (max(rowid)) from f_log"))
+
+(define (find-witticism conn s)
+  (let-values ([(rarest pop) (rarest-token conn s)])
+    (and (integer? pop)
+         (let ([ro (random-offset conn)])
+           (db:query-value conn
+            @string-append{
+SELECT  f_log.text
+FROM    f_log
+WHERE   f_log.text MATCH ?
+AND     f_log.rowid > ?
+ORDER   BY f_log.rowid ASC
+LIMIT   1
+}
+            s ro)))))
+
 (provide make-incubot-server)
 (define (make-incubot-server)
   (define connection (db:sqlite3-connect
@@ -27,6 +65,5 @@
       ['put-string
        (add-sentence connection inp)]
       ['get
-       (dprintf "Pretend I'm retrieving something from ~a" inp)
-       "incubot is on vacation today"])
+       (find-witticism connection (string-join inp " "))])
     ))
