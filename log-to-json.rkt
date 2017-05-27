973db74a668f3f4968f4e3a5938a00ff357b4769
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-05-27 14:22:42 -0700

    Return a slightly-more-useful format.
    More useful, that is, for feeding to Elasticsearch.

--- log-to-json.rkt
+++ log-to-json.rkt
@@ -8,32 +8,40 @@
          (only-in "lexer.rkt" parse-message))
 
 (define/contract (maybe-parse-line l)
-  (string? . -> . (or/c (cons/c string? any/c) false/c))
+  (string? . -> . (or/c (and/c immutable? hash?) false/c))
   (match l
     ;; The log has two different varieties of entry: one is a sort of
     ;; sexp, which we recognize because it begins with a left paren;
     ;; the other is an unparsed string, which we parse via lexer.rkt).
     [(regexp #px"^(.{19}Z) <= (\\(.*\\))" (list _ timestamp sexp))
-     (cons timestamp (to-jsexpr (read  (open-input-string sexp))))]
+     (make-immutable-hasheq
+      `(
+        (timestamp . ,timestamp)
+        ,@(simplify-log (read  (open-input-string sexp)))
+        )
+      )]
     [(regexp #px"^(.{19}Z) <= (\".*\")" (list _ timestamp string))
-     (cons timestamp (to-jsexpr (parse-message (read (open-input-string string)))))]
+     (make-immutable-hasheq
+      `(
+        (timestamp . ,timestamp)
+        ,@(simplify-log (parse-message (read (open-input-string string))))
+        )
+      )]
     [_ #f]))
 
-(define/contract (to-jsexpr alist)
-  (dict? . -> . jsexpr?)
-  (make-immutable-hasheq
-   (append-map
-    (match-lambda
-      ;; I have no idea why the log writes out "params" this
-      ;; verbosely.
-      [(list 'params (list 'param (? bytes? v)) ...)
-       (list (cons 'params (map bytes->string/utf-8 v)))]
+(define (simplify-log alist)
+  (append-map
+   (match-lambda
+     ;; I have no idea why the log writes out "params" this
+     ;; verbosely.
+     [(list 'params (list 'param (? bytes? v)) ...)
+      (list (cons 'params (map bytes->string/utf-8 v)))]
 
-      [(list symbol (? bytes? v))
-       (list (cons symbol (bytes->string/utf-8 v)))]
+     [(list symbol (? bytes? v))
+      (list (cons symbol (bytes->string/utf-8 v)))]
 
-      [_ '()])
-    alist)))
+     [_ '()])
+   alist))
 
 
 (module+ test
@@ -42,47 +50,43 @@
   (check-false (maybe-parse-line "2016-07-13T04:24:14Z Main starting."))
   (check-false (maybe-parse-line "2015-08-23T20:55:35Z => (left-pointing-arrow-only)"))
   (check-equal? (maybe-parse-line "2011-06-03T14:11:54Z <= \":brx!~brx@p4FE11B58.dip.t-dialin.net JOIN :#emacs\"")
-                (cons "2011-06-03T14:11:54Z"
-                      #hasheq((params . ("#emacs"))
-                              (command . "JOIN")
-                              (prefix . "brx!~brx@p4FE11B58.dip.t-dialin.net"))
-                      ))
-  (check-equal? (to-jsexpr '((prefix #"niven.freenode.net")
-                             (command #"001")
-                             (params
-                              (param #"rudybot")
-                              (param #"Welcome to the freenode Internet Relay Chat Network rudybot"))))
-                #hasheq((prefix . "niven.freenode.net")
-                        (command . "001")
-                        (params . ("rudybot" "Welcome to the freenode Internet Relay Chat Network rudybot"))))
+                #hasheq((timestamp . "2011-06-03T14:11:54Z")
+                        (params . ("#emacs"))
+                        (command . "JOIN")
+                        (prefix . "brx!~brx@p4FE11B58.dip.t-dialin.net")))
+  (check-equal? (simplify-log '((prefix #"niven.freenode.net")
+                                (command #"001")
+                                (params
+                                 (param #"rudybot")
+                                 (param #"Welcome to the freenode Internet Relay Chat Network rudybot"))))
+                '((prefix . "niven.freenode.net")
+                  (command . "001")
+                  (params . ("rudybot" "Welcome to the freenode Internet Relay Chat Network rudybot"))))
 
   (check-equal?
    (maybe-parse-line
     "2015-08-23T20:55:35Z <= ((prefix #\"weber.freenode.net\") (command #\"NOTICE\") (params (param #\"*\") (param #\"*** Looking up your hostname...\")))")
-   (cons "2015-08-23T20:55:35Z"
-         #hasheq((prefix . "weber.freenode.net")
-                 (command . "NOTICE")
-                 (params . ("*"  "*** Looking up your hostname...")))))
+   #hasheq((timestamp . "2015-08-23T20:55:35Z")
+           (prefix . "weber.freenode.net")
+           (command . "NOTICE")
+           (params . ("*"  "*** Looking up your hostname..."))))
   (check-equal?
    (maybe-parse-line
     "2010-01-19T03:01:07Z <= \"NOTICE AUTH :*** Checking ident\"")
-   (cons "2010-01-19T03:01:07Z"
-         #hasheq((command . "NOTICE")
-                 (params . ("AUTH" "*** Checking ident")))))
+   #hasheq((timestamp . "2010-01-19T03:01:07Z")
+           (command . "NOTICE")
+           (params . ("AUTH" "*** Checking ident"))))
   (check-equal?
    (maybe-parse-line
     "2011-06-03T17:43:54Z <= ((params (param . #f)))")
-   (cons
-    "2011-06-03T17:43:54Z"
-    #hasheq()
-    ))
+   #hasheq((timestamp . "2011-06-03T17:43:54Z")))
   )
 
 
 (module+ main
-  (call-with-input-file "/mnt/rudybot/big-log.restored"
+  (call-with-input-file "big-log"
     (lambda (inf)
-      (call-with-output-file "/dev/null" #:exists 'append
+      (call-with-output-file "big-log.json" #:exists 'append
         (lambda (outf)
           (for ([(line index) (in-indexed (in-lines inf))])
             (when (zero? (remainder index 5000))
@@ -91,10 +95,9 @@
                              (lambda (e)
                                (printf "~s => ~a~%" line e))]
                             )
-              (match (maybe-parse-line line)
-                [(cons timestamp sexp )
-                 (write-json (list timestamp sexp) outf)
-                 (newline outf)]
-                [_ #f]))
+              (let ([maybe-json (maybe-parse-line line)])
+                (when maybe-json
+                  (write-json maybe-json outf)
+                  (newline outf))))
             ))))
     ))
