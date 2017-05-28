5208194a49abe4b365bc053c32f921b3bd2e732f
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-05-28 16:05:21 +0000

    Simplify batching
    Use concurrent.futures instead of threading, for no particularly good reason.
    
    It shows about 24 minutes ETA at 2% done.

--- line_batcher.py
+++
@@ -1,8 +0,0 @@
-from itertools import islice, chain
-
-
-def batch(iterable, size):
-    sourceiter = iter(iterable)
-    while True:
-        batchiter = islice(sourceiter, size)
-        yield tuple(chain([next(batchiter)], batchiter))
--- upload-json-log-to-elasticsearch.py
+++ upload-json-log-to-elasticsearch.py
@@ -1,52 +1,75 @@
 """Read the log stream produced by log-to-json.rkt, and upload each
 entry to elasticsearch.
 
+We're using elasticsearch's bulk ingestion API, which is documented
+(not very well) at
+https://www.elastic.co/guide/en/elasticsearch/reference/5.4/docs-bulk.html
+
 """
 
-# This is surprsingly slow; I suspect I want to use the "bulk
-# ingestion" API
-# (https://www.elastic.co/guide/en/elasticsearch/reference/5.4/docs-bulk.html)
-
-# To delete the whole mess in order to start over, paste this into https://wat/_plugin/kibana/app/kibana#/dev_tools/console
-# POST messages/message/_delete_by_query?conflicts=proceed
-# {
-#   "query": {
-#     "match_all": {}
-#   }
-# }
-
+import concurrent.futures
 import hashlib
 import json
 import os
-import pprint
-import queue
-import threading
 
-import line_batcher
-
-import elasticsearch
+import elasticsearch            # pip install elasticsearch
 import elasticsearch.helpers
 import progressbar              # pip install progressbar2
 
-# I don't want the actual URL here since its permissions are too lax,
-# and anything in this file will wind up on github
-ELASTICSEARCH_DOMAIN_ENDPOINT = os.getenv('ELASTICSEARCH_DOMAIN_ENDPOINT')
+# To delete the whole mess in order to start over, paste this into https://wat/_plugin/kibana/app/kibana#/dev_tools/console
+"""
+POST messages/message/_delete_by_query?conflicts=proceed
+{
+  "query": {
+    "match_all": {}
+  }
+}
+"""
 
-ELASTICSEARCH_DOMAIN_MESSAGE_CONTAINER = '/messages/message'
+# TODO -- query elasticsearch before starting, to find the newest
+# timestamp; only upload newer items.
+
+# I don't want the actual URL here since the server's permissions are
+# too lax, and anything in this file will wind up on github
+ELASTICSEARCH_DOMAIN_ENDPOINT = os.getenv('ELASTICSEARCH_DOMAIN_ENDPOINT')
 
 
 def short_hash(stuff):
     return hashlib.sha256(stuff).hexdigest()[0:8]
 
 
-def compute_document_url(line):
-    return 'https://{}/{}/{}'.format(
-        ELASTICSEARCH_DOMAIN_ENDPOINT,
-        ELASTICSEARCH_DOMAIN_MESSAGE_CONTAINER,
-        short_hash(line))
+def ingest_one_batch(es, batch_of_lines):
+    index_action_dicts = []
+    for line in batch_of_lines:
+        data = json.loads(line)
+        data.update(
+            {
+                '_index': 'messages',
+                '_type': 'message',
+                '_id': short_hash(line.encode('utf-8'))
+            })
+        index_action_dicts.append(data)
+
+    elasticsearch.helpers.bulk(es, index_action_dicts)
+
+    return sum((len(l) for l in batch))
 
 
-def worker():
+LINES_PER_BATCH = 50000
+NUMBER_OF_WORKER_THREADS = 2
+
+def line_batcher(inf, number_of_lines):
+    batch = []
+    for line in inf:
+        batch.append(line)
+        if len(batch) == number_of_lines:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+
+if __name__ == "__main__":
     es = elasticsearch.Elasticsearch(hosts=[
         {
             'host': ELASTICSEARCH_DOMAIN_ENDPOINT,
@@ -54,49 +77,16 @@ def worker():
             'port': 443,
         }
     ])
-    while True:
-        batch_of_lines = work_queue.get()
-        if batch_of_lines is None:
-            break
-
-        index_action_dicts = []
-        for line in batch_of_lines:
-            data = json.loads(line)
-            data.update(
-                {
-                    '_index': 'messages',
-                    '_type': 'message',
-                    '_id': short_hash(line.encode('utf-8'))
-                })
-            index_action_dicts.append(data)
-
-        elasticsearch.helpers.bulk(es, index_action_dicts)
-
-        work_queue.task_done()
-
-
-if __name__ == "__main__":
-    work_queue = queue.Queue(maxsize=100)
-    threads = [threading.Thread(target=worker) for _ in range(10)]
-    for t in threads:
-        t.start()
 
     with open('big-log.json') as inf:
-        progress = progressbar.ProgressBar(max_value=os.fstat(inf.fileno()).st_size)
+        with progressbar.ProgressBar(max_value=os.fstat(inf.fileno()).st_size) as progress:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=NUMBER_OF_WORKER_THREADS) as executor:
+                futures = []
+                for batch in line_batcher(inf, LINES_PER_BATCH):
+                    f = executor.submit(ingest_one_batch, es, batch)
+                    f.add_done_callback(lambda f: progress.update(progress.value + f.result()))
+                    futures.append(f)
 
-        # Oddly, we can't use "for line in inf:" here, because that
-        # causes the "tell" method to raise an exception.
-        # https://stackoverflow.com/a/42150352/20146
-        for batch in line_batcher.batch(inf, 50000):
-            work_queue.put(batch)
-
-            progress.update(inf.tell())
-
-    work_queue.join()
-
-    # stop workers
-    for t in threads:
-        work_queue.put(None)
-
-    for t in threads:
-        t.join()
+                    if len(futures) == NUMBER_OF_WORKER_THREADS:
+                        concurrent.futures.wait(futures)
+                        futures = []
