3bc4f9f4816774ab50fd976e2f9fe6432b36dac2
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-05-29 13:55:05 -0700

    WIP -- add a bunch of "click" fanciness ...
    ... to (eventually) both writing to _and_ reading from elasticsearch.

--- wit.py
+++ wit.py
@@ -2,10 +2,12 @@ import datetime
 import os
 import pprint
 import random
+import re
 import sys
 
-import click
-import elasticsearch
+import arrow                    # pip install arrow
+import click                    # pip install click
+import elasticsearch            # pip install elasticsearch
 
 
 def _e(*args, **kwargs):
@@ -72,16 +74,54 @@ def find_witticism(input_string, timestamp_of_input, minimum_should_match):
     return hits
 
 
+@click.group()
+def main():
+    pass
+
+
 @click.command()
 @click.argument('text', nargs=-1)
-def main(text):
+def get(text):
     iso_3339_now = datetime.datetime.utcnow().isoformat() + 'Z'
     wits_hits = keep_looking_for_witticism(' '.join(text), iso_3339_now)
     if wits_hits:
         print(random.choice(wits_hits[0:HITS_TO_FETCH])['_source']['text'])
 
 
+def validate_timestamp(ctx, param, value):
+    try:
+        arrow.get(value)
+        return (value)
+    except Exception:
+        raise click.BadParameter(repr(value))
+
+
+def validate_speaker(ctx, param, value):
+    pattern = r'.+!.+@.+'
+    if not re.match(pattern, value):
+        raise click.BadParameter("{!r} doesn't match regex {!r}".format(value, pattern))
+    return value
+
+
+@click.command()
+@click.argument('speaker', required=True, callback=validate_speaker)
+@click.argument('timestamp', required=True, callback=validate_timestamp)
+@click.argument('target', required=True)
+@click.argument('text', nargs=-1)
+def save(speaker, timestamp, target, text):
+    payload = {
+        'speaker': speaker,
+        'timestamp': timestamp,
+        'target': target,
+        'text': text
+    }
+    pprint.pprint(payload)
+
+
 if __name__ == "__main__":
+    main.add_command(get)
+    main.add_command(save)
+
     ELASTICSEARCH_DOMAIN_ENDPOINT = os.getenv('ELASTICSEARCH_DOMAIN_ENDPOINT')
     es = elasticsearch.Elasticsearch(hosts=[
         {
